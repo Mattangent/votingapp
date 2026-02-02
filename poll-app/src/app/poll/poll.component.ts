6f00de5c9546d627733905147035620122ec4970
Matthew Tan <norgaris@gmail.com>
2026-02-01 20:53:37 -0500

    setting up front end side

---
+++ poll-app/src/app/poll/poll.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { PollService } from '../poll.service';
+import { Poll } from '../poll.models';
+@Component({
+  selector: 'app-poll',
+  imports: [],
+  templateUrl: './poll.component.html',
+  styleUrl: './poll.component.css',
+})
+export class PollComponent implements OnInit {
+  polls: Poll[] = [];
+  constructor(private pollService: PollService) {
+
+  }
+
+  ngOnInit(): void {
+    this.loadPolls();
+  }
+
+  loadPolls(){
+    this.pollService.getPolls().subscribe({
+      next: (data) => {
+        this.polls = data;
+      },
+      error: (error) => {
+        console.error("Error fetching polls: ", error);
+      }
+    });
+  }
+
+}
